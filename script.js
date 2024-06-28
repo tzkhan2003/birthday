32eba8c7264e72f35361bc00570d2d4bdb545a30
Tanjim Zaman Khan <67259158+tzkhan2003@users.noreply.github.com>
2024-06-29 00:40:36 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var a=prompt("Enter UserName");
- var b=prompt("Enter password");
-  	if(a== "tanjim" && b=="123")
+var a=prompt("Enter Your Name");
+ var b=prompt("Tanjim's Favourite color?");
+  	if(a== "hafsa" && b=="blue")
   	{
   	    window.location.href="start.html";
   	}
